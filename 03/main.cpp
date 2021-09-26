ee36e9a6ba1f84e4f9b8304de5cb66233920d398
forcespace <force.space@rambler.ru>
2021-09-26 22:32:21 +0300

    Заменил цвета

--- 03/main.cpp
+++ 03/main.cpp
@@ -53,8 +53,8 @@ int main()
     window.clear();
 
     sf::ConvexShape house;
-    house.setFillColor(sf::Color(0xFF, 0, 0xFF));
-    house.setPosition({200, 800});
+    house.setFillColor(sf::Color(77,46,11));
+    house.setPosition({200, 900});
     house.setPointCount(4);
     house.setPoint(0, {800, 0});
     house.setPoint(1, {800, -400});
@@ -62,47 +62,51 @@ int main()
 
     sf::ConvexShape door;
     door.setFillColor(sf::Color(0x0, 0x0, 0x0));
-    door.setPosition({250, 800});
+    door.setPosition({250, 900});
     door.setPointCount(4);
     door.setPoint(0, {150, 0});
     door.setPoint(1, {150, -250});
     door.setPoint(2, {0, -250});
 
     sf::ConvexShape roof;
-    roof.setFillColor(sf::Color(0xFF, 0x80, 0));
-    roof.setPosition({150, 400});
+    roof.setFillColor(sf::Color(93,30,23));
+    roof.setPosition({150, 500});
     roof.setPointCount(4);
     roof.setPoint(0, {900, 0});
     roof.setPoint(1, {650, -150});
     roof.setPoint(2, {250, -150});
 
     sf::ConvexShape pipe1;
-    pipe1.setFillColor(sf::Color(0xFF, 0x0, 0x0));
-    pipe1.setPosition({720, 340});
+    pipe1.setFillColor(sf::Color(59,56,56));
+    pipe1.setPosition({720, 440});
     pipe1.setPointCount(4);
     pipe1.setPoint(0, {65, 0});
     pipe1.setPoint(1, {65, -115});
     pipe1.setPoint(2, {0, -115});
 
     sf::ConvexShape pipe2;
-    pipe2.setFillColor(sf::Color(0xFF, 0x0, 0x0));
-    pipe2.setPosition({700, 240});
+    pipe2.setFillColor(sf::Color(59,56,56));
+    pipe2.setPosition({700, 340});
     pipe2.setPointCount(4);
     pipe2.setPoint(0, {100, 0});
     pipe2.setPoint(1, {100, -60});
     pipe2.setPoint(2, {0, -60});
 
     sf::CircleShape shape1(25);
-    shape1.setPosition({740, 130});
-    shape1.setFillColor(sf::Color(0x0, 0xFF, 0x0));
+    shape1.setPosition({740, 230});
+    shape1.setFillColor(sf::Color(191,191,191));
 
     sf::CircleShape shape2(30);
-    shape2.setPosition({750, 90});
-    shape2.setFillColor(sf::Color(0x0, 0xFF, 0x0));
+    shape2.setPosition({750, 190});
+    shape2.setFillColor(sf::Color(191,191,191));
 
     sf::CircleShape shape3(35);
-    shape3.setPosition({770, 50});
-    shape3.setFillColor(sf::Color(0x0, 0xFF, 0x0));
+    shape3.setPosition({770, 150});
+    shape3.setFillColor(sf::Color(191,191,191));
+
+    sf::CircleShape shape4(40);
+    shape4.setPosition({780, 100});
+    shape4.setFillColor(sf::Color(191,191,191));
 
     while (window.isOpen())
     {
@@ -123,6 +127,7 @@ int main()
         window.draw(shape1);
         window.draw(shape2);
         window.draw(shape3);
+        window.draw(shape4);
         window.display();
     }
 }
